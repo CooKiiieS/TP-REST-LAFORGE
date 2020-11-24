0726b6c556cc6bfad9055e2da80294d96813593f
test <test@244-0>
2020-11-24 11:52:26 +0100

    question 1.5

--- eteindre.sh
+++ eteindre.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl http://10.202.255.252/relay/0?turn=off |jq # Allume la prise
-sleep 5						# Délai de 5s
-curl http://10.202.255.252/meter/0 |jq .power   # Voir sa consommation actuelle
-curl http://10.202.255.252/relay/0 |jq		# Voir si la prise est allumée ou non (ison=true ou false)
+curl --silent http://10.202.255.252/relay/0?turn=off |jq # Allume la prise
+sleep 5					                  # Délai de 5s
+curl --silent http://10.202.255.252/meter/0 |jq .power   # Voir sa consommation actuelle
+curl --silent http://10.202.255.252/relay/0 |jq	  # Voir si la prise est allumée ou non (ison=true ou false)
