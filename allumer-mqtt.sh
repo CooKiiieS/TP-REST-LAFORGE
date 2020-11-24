0c6e1a44bf88b643bdb9a5829215275f6f376e42
test <test@244-0>
2020-11-24 15:02:05 +0100

    question 2.1

---
+++ allumer-mqtt.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mosquitto_pub -h 10.202.0.107 -t shellies/shellyplug-s-6A6534/relay/0/command -m "on" # Envoi l'ordre pour allumer la prise
+sleep 5										      # On attend 5 secondes
+echo "La consommation actuelle est de : `mosquitto_sub -h 10.202.0.107 -t shellies/shellyplug-s-6A6534/relay/0/power -C 1` W" # On récupère le consommation actuelle
