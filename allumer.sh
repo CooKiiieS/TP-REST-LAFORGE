26884e327162d813ddff01bd01670623ecaab054
CooKiiieS <55453083+CooKiiieS@users.noreply.github.com>
2020-11-24 13:08:32 +0100

    Update allumer.sh

--- allumer.sh
+++ allumer.sh
@@ -2,5 +2,5 @@
 
 curl --silent http://10.202.255.252/relay/0?turn=on |jq # Allume la prise
 sleep 5 				                 # Délai de 5s
-curl --silent http://10.202.255.252/meter/0 |jq .power  # Voir sa consommation actuelle
+echo "La consommation est de : `curl --silent http://10.202.255.252/meter/0 |jq .power` W"  # Voir sa consommation actuelle
 curl --silent http://10.202.255.252/relay/0 |jq         # Voir si la prise est allumée ou non (ison=true ou false)
